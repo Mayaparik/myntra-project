48a4e8d45a69ac862b1b8ab96195855cc505c846
mayaparik <mayapareek412@gmail.com>
2025-07-30 10:08:11 +0530

    update something

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ function Hero() {
 
   return (
     <div>
-      <div className='w-full h-[250px] md:h-[600px] lg:h-[800px] relative overflow-hidden pt-16'>
+      <div className='w-full h-[400px] md:h-[600px] lg:h-[800px] relative overflow-hidden pt-16'>
 
         {
           banners.map((image, index) => (
